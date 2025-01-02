2982b8909b4bdc90658f3e088284ad895d23e578
the-dmitry <dzmitrygaponenko@gmail.com>
2025-01-02 06:04:44 +0300

    fix: fix2 LinkButton interface

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,43 +1,37 @@
-import Link from 'next/link';
-import { ButtonHTMLAttributes, ReactNode } from 'react';
+import Link, { LinkProps } from 'next/link';
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant: 'button';
 }
 
-interface CustomLink extends React.ComponentProps<typeof Link> {
+interface CustomLink
+  extends LinkProps,
+    Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> {
   variant: 'link';
-  children?: ReactNode;
 }
 
-type Props = (ButtonProps | CustomLink) & {
-  children: ReactNode;
-  className?: string;
-};
+type Props = (ButtonProps | CustomLink) & { children?: React.ReactNode };
 
-export default function Button({
-  variant = 'button',
-  children,
-  className,
-  ...rest
-}: Props) {
+function isButtonProps(props: Props): props is ButtonProps {
+  return props.variant === 'button';
+}
+
+export default function Button(props: Props) {
+  const { children, className } = props;
   const cssNames = twMerge(
     'flex w-fit select-none items-center justify-center rounded-md bg-orange-500 px-6 py-1 text-center font-bold text-black transition-all hover:bg-orange-400 disabled:cursor-not-allowed disabled:sepia',
     className
   );
-  const component = {
-    button: (
-      <button className={cssNames} {...(rest as Omit<ButtonProps, 'variant'>)}>
-        {children}
-      </button>
-    ),
-    link: (
-      <Link className={cssNames} {...(rest as Omit<CustomLink, 'variant'>)}>
-        {children}
-      </Link>
-    ),
-  }[variant];
 
-  return component;
+  return isButtonProps(props) ? (
+    <button {...props} className={cssNames}>
+      {children}
+    </button>
+  ) : (
+    <Link {...props} className={cssNames}>
+      {children}
+    </Link>
+  );
 }

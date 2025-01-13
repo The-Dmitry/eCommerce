a69fa7eddbb2a75cc383d1ea1916ffd396412227
the-dmitry <dzmitrygaponenko@gmail.com>
2025-01-13 10:41:03 +0300

    fix: fix css 2

--- src/shared/ui/skeleton.tsx
+++ src/shared/ui/skeleton.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge';
+
 interface Props {
   className?: string;
 }
@@ -8,7 +10,12 @@ export default function Skeleton({ className = '' }: Props) {
       className={`relative size-full overflow-hidden rounded-md bg-neutral-900 ${className} `}
     >
       <div className='absolute inset-0'>
-        <div className='animate-shimmer absolute inset-0 -translate-x-full bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900' />
+        <div
+          className={twMerge(
+            'animate-shimmer',
+            'absolute inset-0 -translate-x-full bg-gradient-to-r from-neutral-900 via-neutral-800 to-neutral-900'
+          )}
+        />
       </div>
     </div>
   );

18fa7e075bedef83f9ad336b77cb6a650ff41e41
the-dmitry <dzmitrygaponenko@gmail.com>
2025-01-15 02:08:04 +0300

    fix: fix cart data revoking from cookies 4

--- src/features/cart/cart-button.tsx
+++ src/features/cart/cart-button.tsx
@@ -1,10 +1,15 @@
 import { Routes } from '@/src/shared/constants/routes';
 import { fetchCartData } from '@/src/shared/utils/api/cart/fetch-cart-data';
+import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { CiShoppingCart } from 'react-icons/ci';
 import CartItemsCollector from './cart-items-collector';
+import { COOKIES_DATA } from '@shared/constants/cookies-data';
 
 export default async function CartButton() {
+  const id = cookies().get(COOKIES_DATA.CART_ID)?.value;
+  console.log(id);
+
   let count = 0;
   const items: string[] = [];
   const data = await fetchCartData();

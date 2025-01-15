27a91bf6e463a9e35b80655ba724afe5a3e95836
the-dmitry <dzmitrygaponenko@gmail.com>
2025-01-15 03:32:41 +0300

    fix: fix cart data revoking from cookies 9

--- src/shared/utils/api/cart/initialize-cart.ts
+++ src/shared/utils/api/cart/initialize-cart.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import { COOKIES_DATA } from '@shared/constants/cookies-data';
 import fetchActiveCart from '@shared/utils/api/cart/fetch-active-cart';
 import { NextResponse } from 'next/server';
 
@@ -9,9 +8,5 @@ export default async function initializeCart(
   response: NextResponse
 ) {
   const active_cart = await fetchActiveCart(token);
-  if (active_cart) {
-    response.cookies.set(COOKIES_DATA.CART_ID, active_cart.id);
-    response.cookies.set(COOKIES_DATA.CART_VERSION, `${active_cart.version}`);
-  }
-  return response;
+  return active_cart ? active_cart.id : null;
 }

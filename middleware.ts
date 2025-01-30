7583536b8dafad08560fceb721bf3bfd4cf39291
The-Dmitry <105654988+The-Dmitry@users.noreply.github.com>
2025-01-31 01:07:32 +0300

    Merger develop into main (#21)
    * feat: implement login page
    
    * (#2)token handling (#2)
    
    * feat: implement getting token for anonymous user
    
    * feat: implement token revoking
    
    * registration (#3)
    
    * feat: implement logout
    
    * feat: implement customer registration
    
    * (#4)catalog page (#4)
    
    * feat: implement product filtering, add product page and product cards
    
    * feat: implement filtering by categories, implement search by text, add pagination
    
    * feat: implement sorting
    
    * feat: implement sorting by price
    
    * (#5)product page (#5)
    
    * feat: add product page
    
    * feat: implement filtration only discounted products
    
    * (#6)basic styling (#6)
    
    * feat: add style for product catalog
    
    * style: add style for signIn page
    
    * style: add style for signUp page
    
    * style: add style for catalog filter and search components
    
    * style: style catalog page
    
    * style: style pagination
    
    * style: style spinner
    
    * style: styled product page, minor style fixes
    
    * style: style product page
    
    * (#7)cart (#7)
    
    * feat: implement user cart, implement adding products to cart
    
    * feat: implement removing items from cart, style cart items
    
    * feat: implement promocode applying
    
    * fix: fix cart query
    
    * fix: fix SSG of product page
    
    * (#8)personal (#8)
    
    * feat: implement catalog filter component hiding
    
    * style: style navigation for mobile mode
    
    * refactor: put the URL's into constants
    
    * style: css fixes
    
    * feat: implement password change
    
    * refactor: move form validation into a separate function
    
    * feat: implement personal page, password and personal data change
    
    * feat: implement personal page, password and personal data change
    
    * fix: fix user definition (registered or anonymous)
    
    * fix: fix15
    
    * (#9)main page (#9)
    
    * style: add smooth transition for burger menu
    
    * feat: implement slider with discounted products for main page
    
    * feat: add greeting component on main page
    
    * refactor: refactor button component, add link variant
    
    * feat: implement promo code selector on main page
    
    * feat: add link to github
    
    * fix: fix LinkButton interface
    
    * fix: fix2 LinkButton interface
    
    * fix: fix Button props in sideBar component
    
    * (#10)refactor (#10)
    
    * refactor: mode form to ui folder
    
    * refactor: move server actions into separate files
    
    * docs: update readme
    
    * feat: add modal mode for signIn and signUp pages (#12)
    
    * feat: add modal mode for signIn and signUp pages
    
    * fix: remove unused prop
    
    * fix: fix blank line
    
    * fix: fix scrolling body when modal is opened
    
    * (#14)skeleton (#15)
    
    * feat: add skeleton
    
    * fix: fix css
    
    * fix: fix css 2
    
    * fix: fix cart data revoking from cookies (#19)
    
    * fix: fix cart data revoking from cookies
    
    * fix: fix cart data revoking from cookies 9
    
    * fix fetching cart data on start up
    
    * fix: fix redirecting from personal page on log out
    
    * fix: add priority for greeting image
    
    * fix: fix route caching of personal page (#20)
    
    * fix: fix cheating with user type cookies

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,40 @@
+import fetchActiveCart from '@shared/utils/api/cart/fetch-active-cart';
 import { NextResponse, type NextRequest } from 'next/server';
 import { COOKIES_DATA } from './src/shared/constants/cookies-data';
 import { Routes } from './src/shared/constants/routes';
 import REGISTERED_USER from './src/shared/constants/user-type';
-import fetchActiveCart from './src/shared/utils/api/cart/fetch-active-cart';
 import getAnonymousToken from './src/shared/utils/api/get-anonymous-token';
 
 const routes = [Routes.SIGN_IN, Routes.SING_UP];
 
 export async function middleware(req: NextRequest) {
-  const [token, refreshToken, userType] = [
+  const [token, refreshToken, userType, cartId] = [
     req.cookies.get(COOKIES_DATA.ACCESS_TOKEN),
     req.cookies.get(COOKIES_DATA.REFRESH_TOKEN),
     req.cookies.get(COOKIES_DATA.USER_TYPE),
+    req.cookies.get(COOKIES_DATA.CART_ID),
   ];
 
   if (
-    userType?.value === REGISTERED_USER &&
+    userType?.value &&
     routes.includes(req.nextUrl.pathname.replace(/\/$/, ''))
   ) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
   if (
-    userType?.value !== REGISTERED_USER &&
+    !userType?.value &&
     req.nextUrl.pathname.replace(/\/$/, '') === Routes.PERSONAL
   ) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
+  const response = NextResponse.next();
   if (!(token && refreshToken)) {
     const data = await getAnonymousToken();
     if ('access_token' in data) {
       const { access_token, refresh_token, expires_in, scope } = data;
-      const active_cart = await fetchActiveCart(access_token);
       const isAnonymous = scope.includes('anonymous');
-      const response = NextResponse.next();
       response.cookies.set(COOKIES_DATA.ACCESS_TOKEN, access_token, {
         maxAge: expires_in,
       });
@@ -45,15 +45,18 @@ export async function middleware(req: NextRequest) {
         COOKIES_DATA.USER_TYPE,
         isAnonymous ? '' : REGISTERED_USER
       );
-      if (active_cart) {
-        response.cookies.set(COOKIES_DATA.CART_ID, active_cart.id);
-        response.cookies.set(
-          COOKIES_DATA.CART_VERSION,
-          `${active_cart.version}`
-        );
-      }
 
       return response;
     }
+  }
+
+  if (token && !cartId?.value) {
+    const active_cart = await fetchActiveCart(token.value);
+    if (active_cart) {
+      response.cookies.set(COOKIES_DATA.CART_ID, active_cart.id);
+      response.cookies.set(COOKIES_DATA.CART_VERSION, `${active_cart.version}`);
+    }
+
+    return response;
   }
 }
--- src/shared/utils/api/cart/fetch-active-cart.ts
+++ src/shared/utils/api/cart/fetch-active-cart.ts
@@ -4,7 +4,8 @@ import { BASE_URL } from '@/src/shared/constants/base-url';
 import { CartData } from '@/src/shared/models/CartData';
 import { ResponseError } from '@/src/shared/models/ResponseError';
 
-export default async function fetchActiveCart(token: string) {
+export default async function fetchActiveCart(token?: string) {
+  if (!token) return null;
   const response = await fetch(`${BASE_URL.HOST}/me/active-cart`, {
     headers: {
       'Content-Type': 'application/json',
